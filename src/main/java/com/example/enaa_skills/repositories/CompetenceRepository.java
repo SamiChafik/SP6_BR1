3754e44627aee01ad596fa72572a7d9e5d9e1be0
SamiChafik <sami.chafik00@gmail.com>
2025-07-03 16:26:56 +0100

    Conpetence repo

---
+++ src/main/java/com/example/enaa_skills/repositories/CompetenceRepository.java
@@ -0,0 +1,7 @@
+package com.example.enaa_skills.repositories;
+
+import com.example.enaa_skills.entities.Competence;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompetenceRepository extends JpaRepository<Competence, Long> {
+}
