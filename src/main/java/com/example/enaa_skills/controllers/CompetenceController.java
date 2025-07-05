8da0b05e21a6f9d485e3aa20815c873a5e0c0e80
SamiChafik <sami.chafik00@gmail.com>
2025-07-05 12:22:02 +0100

    new endpoint to export

--- src/main/java/com/example/enaa_skills/controllers/CompetenceController.java
+++ src/main/java/com/example/enaa_skills/controllers/CompetenceController.java
@@ -2,18 +2,27 @@ package com.example.enaa_skills.controllers;
 
 import com.example.enaa_skills.dtos.CompetenceDTO;
 import com.example.enaa_skills.dtos.SubCompetenceDTO;
+import com.example.enaa_skills.entities.Competence;
 import com.example.enaa_skills.services.CompetenceService;
+import com.example.enaa_skills.services.ExceleServices;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping("/competence")
 public class CompetenceController {
     private final CompetenceService service;
+    private final ExceleServices excel;
 
-    public CompetenceController(CompetenceService service) {
+    public CompetenceController(CompetenceService service, ExceleServices excel) {
         this.service = service;
+        this.excel = excel;
     }
 
     @GetMapping("/getAll")
@@ -39,5 +48,24 @@ public class CompetenceController {
     @DeleteMapping("/delete/{id}")
     public void deleteCompetence(@PathVariable Long id){
         service.deleteCompetence(id);
+    }
+
+    @GetMapping("/export/excel")
+    public void exportToExcel(HttpServletResponse response) throws IOException {
+        // 1. Définir le type de contenu de la réponse
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+
+        // 2. Créer un nom de fichier dynamique
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String currentDateTime = dateFormatter.format(new Date());
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=rapportcompetences" + currentDateTime + ".xlsx";
+        response.setHeader(headerKey, headerValue);
+
+        // 3. Récupérer les données à exporter
+        List<Competence> competences = service.getAll2();
+
+        // 4. Appeler le service d'export
+        excel.exportCompetencesToExcel(competences, response.getOutputStream());
     }
 }
